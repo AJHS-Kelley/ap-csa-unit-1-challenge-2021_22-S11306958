f94f8e748a4d552e789ce2e20872b4b5f7baf284
Randy Kaneer <s11306958@students.duvalschools.org>
2021-10-20 09:33:33 -0400

    Cleaned it up some

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,4 +1,4 @@
-// <David Kaneer>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:12>, v3.0
+// <David Kaneer>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:33>, v3.5
 
 import java.util.Scanner;
 
@@ -7,15 +7,16 @@ public class DamageDealt {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("What weapon are you using?");
         String weaponType = myScanner.nextLine();
-
+        System.out.println(" ");
         System.out.println("How many attacks would you like to make?");
         int amountOfAttacksMade = myScanner.nextInt();
-
+        System.out.println(" ");
         System.out.println("How much damage will each attack do?");
         double damagePerAttack = myScanner.nextDouble();
-
+        System.out.println(" ");
         System.out.println("What Enemy are you attacking?");
         String enemyType = myScanner.nextLine();
+        System.out.println(" ");
         double damageDealt = amountOfAttacksMade * damagePerAttack;
         double dps = damageDealt / 4;
         System.out.println("You dealt " + damageDealt + " to the " + enemyType + " with your " + weaponType
