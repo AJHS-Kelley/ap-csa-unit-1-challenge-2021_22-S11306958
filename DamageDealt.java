f368ede08466138a6354b8b1238d84e598c6377d
Randy Kaneer <s11306958@students.duvalschools.org>
2021-10-20 08:54:20 -0400

    Asks the user to type the amount of attacks they want to do

---
+++ DamageDealt.java
@@ -0,0 +1,12 @@
+// <David Kaneer>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <8:23>, v0.5
+
+import java.util.Scanner;
+
+public class DamageDealt {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("How many attacks would you like to make?");
+        System.out.println(" ");
+
+    }
+}
